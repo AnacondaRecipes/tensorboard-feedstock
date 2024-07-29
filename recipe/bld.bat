bc8fbbbd5eb1da27016552ca83329ef2ecad1f5a
Charles Bousseau <cbousseau@anaconda.com>
2024-07-29 13:39:26 -0400

    update to 2.17.0

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,5 @@
-move tensorboard-%PKG_VERSION%-py3-none-any.whl.dummy tensorboard-%PKG_VERSION%-py3-none-any.whl
-%PYTHON% -m pip install --no-deps tensorboard-%PKG_VERSION%-py3-none-any.whl
+%PYTHON% -m pip install --no-deps --ignore-installed .\tensorboard-%PKG_VERSION%-py3-none-any.whl
+if errorlevel 1 exit 1
+REM Remove the strict protobuf requirement. Conda will manage it.
+sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' %SP_DIR%\tensorboard-%PKG_VERSION%.dist-info\METADATA
+if errorlevel 1 exit 1
