08c71b44d1d79300ab588d4a021a7f16867b162d
Eric Lundby <Eric.Lundby@gmail.com>
2025-10-14 16:21:53 -0600

    v2.20

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 %PYTHON% -m pip install --no-deps --ignore-installed --no-build-isolation .\tensorboard-%PKG_VERSION%-py3-none-any.whl
 if errorlevel 1 exit 1
-REM Remove the strict protobuf requirement. Conda will manage it.
-sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' %SP_DIR%\tensorboard-%PKG_VERSION%.dist-info\METADATA
-if errorlevel 1 exit 1
+@REM REM Remove the strict protobuf requirement. Conda will manage it.
+@REM sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' %SP_DIR%\tensorboard-%PKG_VERSION%.dist-info\METADATA
+@REM if errorlevel 1 exit 1
